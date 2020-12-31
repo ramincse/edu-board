7820c3c4a24a2dc8499a685c2ddfe7bb2f40bb15
ramincse <ramin.cse2009@gmail.com>
2020-12-31 20:04:10 +0600

    Auth System Done

--- admin/templates/menu.php
+++ admin/templates/menu.php
@@ -1,36 +1,32 @@
 <!-- .aside -->
-                <aside class="bg-black aside-md hidden-print hidden-xs" id="nav">
-                    <section class="vbox">
-                        <section class="w-f scrollable">
-                            <div class="slim-scroll" data-height="auto" data-disable-fade-out="true" data-distance="0" data-size="10px" data-railOpacity="0.2">
-                                <div class="clearfix wrapper dk nav-user hidden-xs">
-                                    <div class="dropdown">
-                                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                            <span class="thumb avatar pull-left m-r">
+<aside class="bg-black aside-md hidden-print hidden-xs" id="nav">
+    <section class="vbox">
+        <section class="w-f scrollable">
+            <div class="slim-scroll" data-height="auto" data-disable-fade-out="true" data-distance="0" data-size="10px" data-railOpacity="0.2">
+                <div class="clearfix wrapper dk nav-user hidden-xs">
+                    <div class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                            <span class="thumb avatar pull-left m-r">
 
-                                                <img src="images/<?php echo $_SESSION['photo']; ?>" class="dker" alt="...">
+                                <img src="images/<?php echo $_SESSION['photo']; ?>" class="dker" alt="...">
 
-                                                <i class="on md b-black"></i>
-                                            </span>
-                                            <span class="hidden-nav-xs clear">
-                                                <span class="block m-t-xs">
-                                                    <strong class="font-bold text-lt"><?php echo $_SESSION['name']; ?></strong> <b class="caret"></b>
-                                                </span>
-                                                <span class="text-muted text-xs block"><?php echo $_SESSION['cell']; ?></span>
-                                            </span>
-                                        </a>
-                                        <ul class="dropdown-menu animated fadeInRight m-t-xs">
-                                            <li> <span class="arrow top hidden-nav-xs"></span> <a href="#">Settings</a> </li>
-                                            <li> <a href="profile.html">Profile</a> </li>
-                                            <li>
-                                                <a href="#"> <span class="badge bg-danger pull-right">3</span> Notifications </a>
-                                            </li>
-                                            <li> <a href="docs.html">Help</a> </li>
-                                            <li class="divider"></li>
-                                            <li> <a href="modal.lockme.html" data-toggle="ajaxModal">Logout</a> </li>
-                                        </ul>
-                                    </div>
-                                </div>
+                                <i class="on md b-black"></i>
+                            </span>
+                            <span class="hidden-nav-xs clear">
+                                <span class="block m-t-xs">
+                                    <strong class="font-bold text-lt"><?php echo $_SESSION['name']; ?></strong> <b class="caret"></b>
+                                </span>
+                                <span class="text-muted text-xs block"><?php echo $_SESSION['cell']; ?></span>
+                            </span>
+                        </a>
+                        <ul class="dropdown-menu animated fadeInRight m-t-xs">
+                            <li> <span class="arrow top hidden-nav-xs"></span> <a href="#">Settings</a> </li>
+                            <li> <a href="profile.html">Profile</a> </li>
+                            <li class="divider"></li>
+                            <li> <a href="modal.lockme.html" data-toggle="ajaxModal">Logout</a> </li>
+                        </ul>
+                    </div>
+                </div>
 
 
 
@@ -48,27 +44,19 @@
                                     <div class="text-muted text-sm hidden-nav-xs padder m-t-sm m-b-sm">Start</div>
                                     <ul class="nav nav-main" data-ride="collapse">
                                         <li class="active">
-                                            <a href="index.html" class="auto"> <i class="i i-statistics icon"> </i> <span class="font-bold">Overview</span> </a>
+                                            <a href="dashboard.php" class="auto"> <i class="i i-statistics icon"> </i> <span class="font-bold">Dashboard</span> </a>
                                         </li>
                                         <li>
-                                            <a href="#" class="auto"> <span class="pull-right text-muted"> <i class="i i-circle-sm-o text"></i> <i class="i i-circle-sm text-active"></i> </span> <b class="badge bg-danger pull-right">4</b> <i class="i i-stack icon"> </i> <span class="font-bold">Layouts</span> </a>
-                                            <ul class="nav dk">
-                                                <li>
-                                                    <a href="layout-color.html" class="auto"> <i class="i i-dot"></i> <span>Color option</span> </a>
-                                                </li>
-                                                <li>
-                                                    <a href="layout-hbox.html" class="auto"> <i class="i i-dot"></i> <span>Hbox layout</span> </a>
-                                                </li>
-                                                <li>
-                                                    <a href="layout-boxed.html" class="auto"> <i class="i i-dot"></i> <span>Boxed layout</span> </a>
-                                                </li>
-                                                <li>
-                                                    <a href="layout-fluid.html" class="auto"> <i class="i i-dot"></i> <span>Fluid layout</span> </a>
-                                                </li>
-                                            </ul>
+                                            <a href="dashboard.php" class="auto"> <i class="i i-statistics icon"> </i> <span class="font-bold">Students</span> </a>
                                         </li>
-
+                                        <li>
+                                            <a href="dashboard.php" class="auto"> <i class="i i-statistics icon"> </i> <span class="font-bold">Users</span> </a>
+                                        </li>
+                                        <li>
+                                            <a href="dashboard.php" class="auto"> <i class="i i-statistics icon"> </i> <span class="font-bold">Results</span> </a>
+                                        </li>                                        
                                     </ul>
+
                                     <div class="line dk hidden-nav-xs"></div>
                                     <div class="text-muted text-xs hidden-nav-xs padder m-t-sm m-b-sm">Lables</div>
                                     <ul class="nav">
